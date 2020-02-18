c90b0ddcd89d57a4b7421c163e24377592062caa
Leonardo Elnisky <leonardoelnisky@hotmail.com>
2020-02-18 14:35:22 -0300

    Seção 2 em andamento

---
+++ 02 - dom/desafio-01/app.js
@@ -0,0 +1,9 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        nome: 'Leonardo',
+        idade: 22,
+        random: Math.random(1),
+        source: 'https://i.ytimg.com/vi/DYJcvRwJZWc/hqdefault.jpg',
+    }
+})
