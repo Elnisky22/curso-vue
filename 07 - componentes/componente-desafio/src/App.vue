45e6ac41c02b8b4ef85eaece9767ab1243c8c1d5
Leonardo Elnisky <leonardoelnisky@hotmail.com>
2020-03-03 14:39:25 -0300

    Seção 7 finalizada

---
+++ 07 - componentes/componente-desafio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<v-app>
+		<NavigationDrawer/>
+
+		<Toolbar/>
+
+		<Content/>
+
+		<Footer/>
+	</v-app>
+</template>
+
+<script>
+import NavigationDrawer from './components/template/Navigation-Drawer.vue';
+import Toolbar from './components/template/Toolbar.vue';
+import Content from './components/widgets/Content.vue';
+import Footer from './components/template/Footer.vue';
+
+export default {
+	components: {
+		NavigationDrawer,
+		Toolbar,
+		Content,
+		Footer
+	},
+}
+</script>
