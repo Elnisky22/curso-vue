2d9d26c8619cdbcd08d0e2d4a9e2b4e2c64124e4
Leonardo Elnisky <leonardoelnisky@hotmail.com>
2020-03-03 11:16:46 -0300

    Seção 6 finalizada

---
+++ 06 - vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        {{ titulo }}
+        <button @click="titulo += '#'">Adicionar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            titulo: 'Teste Data usando VueJS!'
+        }
+    }
+};
+</script>
+
+<style>
+    #app {
+        background-color: chocolate;
+        color: #fff;
+    }
+</style>
