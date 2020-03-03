7796ab5489cc5ef1a1cb44fc03e91fbf3df8b44a
Leonardo Elnisky <leonardoelnisky@hotmail.com>
2020-03-03 16:21:23 -0300

    Seção 8 em andamento

---
+++ 08 - componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong>{{ inverterNome() }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+        <button @click="reiniciarFn()">Reiniciar Nome (CallBack)</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            // required: true
+            default: 'Anônimo'
+        },
+        reiniciarFn: Function
+    },
+    data() {
+        return {
+            nomeAlternativo: this.nome
+        }
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('');
+        },
+        reiniciarNome() {
+            this.nome = 'Pedro';
+            this.$emit('nomeMudou', this.nome);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
