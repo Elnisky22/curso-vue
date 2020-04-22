fb094743dcc992fc6920e806475a64d9768d379e
Elnisky <leonardoelnisky@hotmail.com>
2020-04-22 11:58:38 -0300

    Seção 18 em andamento

---
+++ 18 - projeto-03-acoes/stock-trader/src/components/stocks/Stocks.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-layout row wrap>
+        <Stock v-for="s in stocks" :key="s.id" :stock="s"/>
+    </v-layout>
+</template>
+
+<script>
+    import Stock from './Stock'
+    
+    export default {
+        components: {
+            Stock
+        },
+        computed: {
+            stocks() {
+                return this.$store.getters.stocks
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
